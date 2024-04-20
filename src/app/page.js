ebceee931a7e268dc3bae5f43aa39922265dae92
do-yoon <do.yoon.like.cheeze@gmail.com>
2024-04-20 22:44:04 +0900

    added some css

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,29 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Search</div>
-      <div>
-        <span>Inbox</span>
-        <span>0</span>
+    <main className="flex flex-col justify-between p-10">
+      <div className="search">Search</div>
+      <div className="nav">
+        <div>
+          <span>Inbox</span>
+          <span id="inbox-number">0</span>
+        </div>
+        <div>
+          <span>Calendar</span>
+        </div>
+        <div>
+          <span>Upcoming</span>
+        </div>
+        <div>
+          <span>Anytime</span>
+        </div>
+        <div>
+          <span>Someday</span>
+        </div>
+        <div>
+          <span>Logbook</span>
+        </div>
       </div>
-      <div>Calendar</div>
-      <div>Upcoming</div>
-      <div>Anytime</div>
-      <div>Someday</div>
-      <div>Logbook</div>
     </main>
   );
 }
