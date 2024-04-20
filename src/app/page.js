f77810c1135c33e958b13adf29ea027bc354c605
do-yoon <do.yoon.like.cheeze@gmail.com>
2024-04-20 23:35:48 +0900

    added onclick event
    * changed div to button
    * added input text

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,39 @@
+'use client'
+
 import Image from "next/image";
+import {url} from "../util/static"
+
+function onClickNav(e, _id) {
+  e.preventDefault();
+  window.location=`${url}/${_id}`
+}
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-between p-10">
-      <div className="search">Search</div>
+      <div className="search">
+        <input type="text" placeholder="Search"></input>
+      </div>
       <div className="nav">
-        <div>
+        <button onClick={(e)=>onClickNav(e, "inbox")}>
           <span>Inbox</span>
           <span id="inbox-number">0</span>
-        </div>
-        <div>
+        </button>
+        <button onClick={(e)=>onClickNav(e, "inbox")}>
           <span>Calendar</span>
-        </div>
-        <div>
+        </button>
+        <button onClick={(e)=>onClickNav(e, "calendar")}>
           <span>Upcoming</span>
-        </div>
-        <div>
+        </button>
+        <button onClick={(e)=>onClickNav(e, "upcoming")}>
           <span>Anytime</span>
-        </div>
-        <div>
+        </button>
+        <button onClick={(e)=>onClickNav(e, "anytime")}>
           <span>Someday</span>
-        </div>
-        <div>
+        </button>
+        <button onClick={(e)=>onClickNav(e, "logbook")}>
           <span>Logbook</span>
-        </div>
+        </button>
       </div>
     </main>
   );
